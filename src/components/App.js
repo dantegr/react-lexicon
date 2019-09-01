4892454369c12f1ef5ab1b081c5eafe2f1005385
unknown <tampantelis@gmail.com>
2019-09-01 19:08:02 +0200

    arrange folder structure and created a basic search bar

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div class="wrap">
+      <div class="search">
+        <input type="text" class="searchTerm" placeholder="What are you looking for?" />
+        <button type="submit" class="searchButton">
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
+  </div>
+  );
+}
+
+export default App;
